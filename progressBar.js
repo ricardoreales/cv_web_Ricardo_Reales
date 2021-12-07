ee779c4d3fdf050d383d886eed8d48c2e8321d86
ricardoreales <ricardorealesabrams@gmail.com>
2021-12-07 14:58:27 -0300

    se agregaron las barras de porcentaje de conocimientos, tambien se agrego al codigo @extend para el codigo de los avatars

---
+++ progressBar.js
@@ -0,0 +1,64 @@
+function updateprogressBar(progressBar, value) {
+    value = Math.round (value)
+    const progressFill = progressBar.querySelector(".progress__fill")
+    const progressText = progressBar.querySelector(".progress__text")
+    progressFill.style.width = value+'%'
+    progressText.textContent = value+'%' 
+}
+
+function scrollFunction() {
+    const scroll = (document.body.scrollTop) ? document.body.scrollTop : document.documentElement.scrollTop
+    const elemProgress = document.querySelectorAll(".progress")
+
+    if ( scroll > 150 && scroll < 1000 ){
+        elemProgress.forEach(function(element) {
+            updateprogressBar(element, element.getAttribute("data-value")) 
+        });
+    }else{
+        elemProgress.forEach(function(element) {
+            updateprogressBar(element,0) 
+        });
+    }
+}
+
+window.onscroll = function() {scrollFunction()}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function saludar(nombre, apellido) {
+    const mySectionIntro = document.querySelector(".intro")
+    const titulo = mySectionIntro.querySelector("h1")
+    titulo.textContent = nombre+" "+apellido
+}
+
+const firstname = "Beatriz"
+const lastname = "Gimenez"
+saludar(firstname,lastname)
+
+*/
+
+
+
+
+
+
+
+
+
