5a2ecd23dee455c854a7bc2f7287bffd7795e23a
ricardoreales <ricardorealesabrams@gmail.com>
2021-12-20 14:18:17 -0300

    prueba background 7

--- progressBar.js
+++ progressBar.js
@@ -1,27 +1,28 @@
-// function updateprogressBar(progressBar, value) {
-//     value = Math.round (value)
-//     const progressFill = progressBar.querySelector(".progress__fill")
-//     const progressText = progressBar.querySelector(".progress__text")
-//     progressFill.style.width = value+'%'
-//     progressText.textContent = value+'%' 
-// }
+function updateprogressBar(progressBar, value) {
+    value = Math.round (value)
+    const progressFill = progressBar.querySelector(".progress__fill")
+    const progressText = progressBar.querySelector(".progress__text")
+    progressFill.style.width = value+'%'
+    progressText.textContent = value+'%' 
+}
 
-// function scrollFunction() {
-//     const scroll = (document.body.scrollTop) ? document.body.scrollTop : document.documentElement.scrollTop
-//     const elemProgress = document.querySelectorAll(".progress")
+function scrollFunction() {
+    const scroll = (document.body.scrollTop) ? document.body.scrollTop : document.documentElement.scrollTop
+    const elemProgress = document.querySelectorAll(".progress")
+    console.log(scroll)
 
-//     if ( scroll > 150 && scroll < 1000 ){
-//         elemProgress.forEach(function(element) {
-//             updateprogressBar(element, element.getAttribute("data-value")) 
-//         });
-//     }else{
-//         elemProgress.forEach(function(element) {
-//             updateprogressBar(element,0) 
-//         });
-//     }
-// }
+    if ( scroll > 500 && scroll < 3000 ){
+        elemProgress.forEach(function(element) {
+            updateprogressBar(element, element.getAttribute("data-value")) 
+        });
+    }else{
+        elemProgress.forEach(function(element) {
+            updateprogressBar(element,0) 
+        });
+    }
+}
 
-// window.onscroll = function() {scrollFunction()}
+window.onscroll = function() {scrollFunction()}
 
 
 
